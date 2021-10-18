f1a12645d8de6558aa2669d86d95218c52052587
nlouis56 <nataniel-.louis@epitech.eu>
2021-10-18 12:59:58 +0200

    adding support for json config files + new commands
    adding the basic support of a json config file in the optic of making a "hot reload" possible for the ping responses and such

--- botv2.py
+++ botv2.py
@@ -4,18 +4,39 @@
 import os
 import discord
 import random
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-ping_response = open('ping_response.txt', encoding="utf8").read().splitlines()
+with open('settings.json') as f:
+    data = json.load(f)
+responses = data["dialogs"]["responses"]
+superusers = data["userinfo"]["superusers"]
 
 help_message = discord.Embed(title="L'aide de zeubi", description="vraiment joli ce truc")
 help_message.add_field(name="Commandes :", value="\n>spam <quantité> <mot>\n>test\n>invite\n")
 help_message.set_footer(text="et puis voilà hein faut pas trop en demander non plus")
 
+permissions_error = discord.Embed(title="Erreur !", description="", color=0xFC0303)
+permissions_error.add_field(name="T'as pas les perms", value="Erreur de permissions, tu ne peut pas éxecuter cette commande")
+permissions_error.set_footer(text="cheh mdr")
+
 client = discord.Client()
+
+def load_assets() :
+    global ping_response
+    with open('settings.json') as f:
+        data = json.load(f)
+    with open(responses, encoding="utf8") as fresp :
+        ping_response = fresp.read().splitlines()
+
+def perms_check(user_id) -> bool:
+    if user_id in superusers :
+        return (True)
+    else :
+        return (False)
 
 async def spam(message) :
     txt = ' '.join(message.content.split()[2:])
@@ -49,21 +70,43 @@ async def man_bash(message) :
             index = index  + n
 
 async def commands_manager(message) :
-    if ">test" in message.content and message.author != client.user :
-        await message.channel.send("tester c'est douter")
-    elif ">spam" in message.content and message.author != client.user :
-        await spam(message)
-    elif ">help" in message.content and message.author != client.user  :
+    if message.content.startswith(">test") :
+        if perms_check(message.author.mention) :
+            await message.channel.send("tester c'est douter")
+        else :
+            await message.channel.send(embed=permissions_error)
+    elif message.content.startswith(">help") :
         help_message.set_thumbnail(url=message.guild.icon_url)
         await message.channel.send(embed=help_message)
-    elif ">invite" in message.content and message.author != client.user  :
-        await message.channel.send("voilà le lien d'invitation espèce de bg : https://discord.com/api/oauth2/authorize?client_id=890884345018069032&permissions=8&scope=bot")
+    elif message.content.startswith(">invite") :
+        await message.channel.send("voilà le lien d'invitation espèce de bg : \
+            https://discord.com/api/oauth2/authorize?client_id=890884345018069032&permissions=8&scope=bot")
+    elif message.content.startswith(">spam") :
+        await spam(message)
+    elif message.content.startswith(">say") :
+        txt = ' '.join(message.content.split()[2:])
+        try :
+            n = int(message.content.split()[1])
+        except (ValueError) :
+            await message.channel.send("c'est pas comme ça que ça s'utilise <:1Head:814062704355704853>\nps : si tu demande gentiment je peux te donner de l'aide")
+            return
+        channel = client.get_channel(n)
+        await channel.send(txt)
+    elif message.content.startswith(">killbot") :
+        if perms_check(message.author.mention) :
+            await message.channel.send("c'est la fin de moi, ciao les gens")
+            print(">killbot received, ending program")
+            os._exit(0)
+        else :
+            await message.channel.send(embed=permissions_error)
+            await message.channel.send("sal fou mdrr")
 
 @client.event
 async def on_ready():
     print(f"{client.user} est connecté à Discord !\nserveurs rejoints:")
     for guild in client.guilds:
         print(f"{guild.name} - id :{guild.id}")
+    load_assets()
 
 @client.event
 async def on_message(message) :
