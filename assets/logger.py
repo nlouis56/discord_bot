b1c395be065bacfd6badfee0ec70b19ba84c74b8
nlouis56 <nataniel-.louis@epitech.eu>
2021-12-17 16:28:36 +0100

    modifying the logger
    now saving all the logs in a dedicated folder to avoid polluting the working directory with archives

--- assets/logger.py
+++ assets/logger.py
@@ -2,24 +2,30 @@
 #coding=utf8
 
 from datetime import datetime
-from os import getpid, path, remove
+from os import getpid, path, remove, mkdir, getcwd
 import psutil
 import gzip
 import shutil
 
 def startup_log() :
-    if path.isfile('log.txt') :
+    cwd = getcwd()
+    logpath = path.join(cwd, "logs")
+    latestpath = path.join(logpath, "latest.txt")
+    print (f"not a directory, path = {logpath}")
+    if not path.isdir(logpath) :
+        mkdir(logpath)
+    if path.isfile(latestpath) :
         now = datetime.now()
         timestr = now.strftime("%d-%m-%Y")
-        archive_name = f"log{timestr}.txt.gz"
+        archive_name = path.join(logpath, f"log{timestr}.txt.gz")
         i = 1
-        with open("log.txt", 'rb') as f_in:
-            while path.isfile(archive_name) :
+        with open(latestpath, 'rb') as f_in:
+            while path.isfile(path.join(logpath, archive_name)) :
                 archive_name = f"log{timestr}-{i}.txt.gz"
                 i = i + 1
-            with gzip.open(archive_name, 'wb') as f_out:
+            with gzip.open(path.join(logpath, archive_name), 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
-        remove("log.txt")
+        remove(latestpath)
         return
     else :
         return
@@ -28,22 +34,25 @@ def writelog(title: str ="LOG", message: str ="void", error: bool =False, perfre
     current = psutil.Process(getpid())
     mem = psutil.virtual_memory().percent
     cpu = psutil.cpu_percent(interval=None)
-    m = round(((current.memory_info().rss) / 1000000), 2)
     now = datetime.now()
     timestr = now.strftime("%d/%m/%Y %H:%M:%S")
-    if path.isfile('log.txt') :
-        with open('log.txt', 'a', encoding="utf-8") as f:
+    cwd = getcwd()
+    logpath = path.join(cwd, "logs")
+    latestpath = path.join(logpath, "latest.txt")
+
+    if path.isfile(latestpath) :
+        with open(latestpath, 'a', encoding="utf-8") as f:
             if error:
                 f.write(f"/!\\ ERROR - [{timestr}] - {title} - {message}\n")
             elif perfreport :
-                f.write(f"[{timestr}] - PERFORMANCE (normal) - mem global : {mem}% - mem program : {m} MB - cpu global : {cpu}%\n")
+                f.write(f"[{timestr}] - PERFORMANCE (normal) - mem global : {mem}% - mem program : {round(((current.memory_info().rss) / 1000000), 2)} MB - cpu global : {cpu}%\n")
             else :
                 f.write(f"[{timestr}] - {title} - {message}\n")
     else :
-        with open('log.txt', 'w', encoding="utf-8") as f:
+        with open(latestpath, 'w', encoding="utf-8") as f:
             if error:
                 f.write(f"/!\\ ERROR - [{timestr}] - {title} - {message}\n")
             elif perfreport :
-                f.write(f"[{timestr}] - PERFORMANCE (normal) - mem global : {mem}% - mem program : {m} MB - cpu global : {cpu}%\n")
+                f.write(f"[{timestr}] - PERFORMANCE (normal) - mem global : {mem}% - mem program : {round(((current.memory_info().rss) / 1000000), 2)} MB - cpu global : {cpu}%\n")
             else :
                 f.write(f"[{timestr}] - {title} - {message}\n")
